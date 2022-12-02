27f1f633bee89e209772b618558cc47e62bcce20
Freesoul1982 <alex.v.2007@gmail.com>
2022-12-02 12:20:45 +0200

    Initial

--- Go/goSDK.go
+++ Go/goSDK.go
@@ -1,1 +1,24 @@
 package main
+
+import "C"
+
+//export GoCrossCompileExample
+func GoCrossCompileExample(stringValue *C.char, intValue C.int, resultSize *C.int, error *C.char) *C.char {
+
+	//Converting c string to go string
+	goString := C.GoString(stringValue)
+
+	//Converting
+	goInt := C.GoInt(intValue)
+	println("Received String: ", goString)
+	println("Received Int: ", goInt)
+	newGoString := "Go" + goString
+	errorString := "go error"
+	*error = C.CString(errorString)
+	*resultSize = C.int(len(newGoString))
+
+	result := C.CString(newGoString)
+	return result
+}
+
+func main() {}
